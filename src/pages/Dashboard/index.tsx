3a048d24116cfb27c28a402cd2caed56ba1f977a
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-09-30 16:26:11 -0300

    feat: add styles to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImage from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12599575?s=460&u=64383f999caa2522e7bb331bacab1439d3ff714c&v=4"
+            alt="Agnaldo Burgo Junior"
+          />
+          <div>
+            <strong>agnaldoburgojr/todo-app</strong>
+            <p>Minha primeira todo list</p>
+          </div>
+          <FiChevronRight size={28} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12599575?s=460&u=64383f999caa2522e7bb331bacab1439d3ff714c&v=4"
+            alt="Agnaldo Burgo Junior"
+          />
+          <div>
+            <strong>agnaldoburgojr/todo-app</strong>
+            <p>Minha primeira todo list</p>
+          </div>
+          <FiChevronRight size={28} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12599575?s=460&u=64383f999caa2522e7bb331bacab1439d3ff714c&v=4"
+            alt="Agnaldo Burgo Junior"
+          />
+          <div>
+            <strong>agnaldoburgojr/todo-app</strong>
+            <p>Minha primeira todo list</p>
+          </div>
+          <FiChevronRight size={28} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
