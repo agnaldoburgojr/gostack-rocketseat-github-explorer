e0db07230593c366a53f251b9bcbec66feb1ffdc
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-01 09:00:54 -0300

    feat: add repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImage from '../../assets/logo.svg';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +10,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImage} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/12599575?s=460&u=64383f999caa2522e7bb331bacab1439d3ff714c&v=4"
+            alt="Agnaldo"
+          />
+          <div>
+            <strong>AgnaldoBurgoJunior/todo-app</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to={`/repository`}>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={28} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
