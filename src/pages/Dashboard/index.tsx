4751fa9b2755ae78f6278cda858c870c1985a428
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-01 08:28:01 -0300

    feat: add link to repository page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 import logoImage from '../../assets/logo.svg';
@@ -73,7 +74,10 @@ const Dashboard: React.FC = () => {
       {inputError && <InputError>{inputError}</InputError>}
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -83,7 +87,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={28} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
